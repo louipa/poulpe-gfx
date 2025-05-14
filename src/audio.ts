440de25285dd996c999ab1a83653b84bb516557f
Louis Painter <louis.painter49@gmail.com>
2025-05-14 17:43:37 +0200

    🐛 fix sounds not starting unless user interacts

--- src/audio.ts
+++ src/audio.ts
@@ -8,6 +8,7 @@ export default class AudioManager {
   private fadeDuration: number = 1.0;
   private fadeStartTime: number = 0;
   private isFading: boolean = false;
+  private soundsReady: boolean = false;
 
   constructor(camera: THREE.Camera) {
     this.listener = new THREE.AudioListener();
@@ -17,22 +18,39 @@ export default class AudioManager {
     this.waterBubblesSound = new THREE.Audio(this.listener);
 
     const audioLoader = new THREE.AudioLoader();
-    audioLoader.load("water_flowing.mp3", (buffer) => {
+    audioLoader.load("/poulpe-gfx/water_flowing.mp3", (buffer) => {
       this.waterFlowingSound.setBuffer(buffer);
       this.waterFlowingSound.setLoop(true);
       this.waterFlowingSound.setVolume(0.5);
-      this.waterFlowingSound.play();
     });
 
-    audioLoader.load("water_bubbles.mp3", (buffer) => {
+    audioLoader.load("/poulpe-gfx/water_bubbles.mp3", (buffer) => {
       this.waterBubblesSound.setBuffer(buffer);
       this.waterBubblesSound.setLoop(true);
       this.waterBubblesSound.setVolume(0);
-      this.waterBubblesSound.play();
     });
+
+    const startAudio = () => {
+      if (!this.soundsReady) {
+        this.waterFlowingSound.play();
+        this.waterBubblesSound.play();
+        this.soundsReady = true;
+        document.removeEventListener("click", startAudio);
+        document.removeEventListener("keydown", startAudio);
+        document.removeEventListener("touchstart", startAudio);
+      }
+    };
+
+    document.addEventListener("click", startAudio);
+    document.addEventListener("keydown", startAudio);
+    document.addEventListener("touchstart", startAudio);
   }
 
   public update(_delta: number): void {
+    if (!this.soundsReady) {
+      return;
+    }
+
     if (this.isFading) {
       const elapsedTime = (Date.now() - this.fadeStartTime) / 1000;
       const fadeProgress = Math.min(elapsedTime / this.fadeDuration, 1);
